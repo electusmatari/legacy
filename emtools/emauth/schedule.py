256defff03557d05d867fce1a43c3a174c811094
arkady <arkady@8b78aa87-fdc0-4a8b-ab22-a370a497df50>
2011-10-29 10:55:00 +0000

    Remove schedule framework
    git-svn-id: http://devel.electusmatari.com/svn/trunk@29 8b78aa87-fdc0-4a8b-ab22-a370a497df50

--- emtools/emauth/schedule.py
+++
@@ -1,5 +0,0 @@
-import datetime
-
-from emtools.emauth.userauth import authenticate_users
-
-SCHEDULE = [("api-auth", authenticate_users, 60*12)]
--- emtools/intel/schedule.py
+++
@@ -1,5 +0,0 @@
-import datetime
-
-from emtools.intel.views import update_all
-
-SCHEDULE = [('intel-update', update_all, 60*12)]
