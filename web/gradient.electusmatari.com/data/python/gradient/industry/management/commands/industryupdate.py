b9b6a2caa55e905ca35e6f94fd9bf1c82d79856d
Jorgen Schaefer <forcer@forcix.cx>
2013-06-12 10:30:24 +0200

    industryupdate: Actually configure logging ...

--- web/gradient.electusmatari.com/data/python/gradient/industry/management/commands/industryupdate.py
+++ web/gradient.electusmatari.com/data/python/gradient/industry/management/commands/industryupdate.py
@@ -1,7 +1,10 @@
+import logging
+
 from django.core.management.base import BaseCommand
 
 from gradient.industry.utils import update
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        logging.basicConfig()
         update()
